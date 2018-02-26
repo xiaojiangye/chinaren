254049946f045f9df2c12798ea7be448ec76269d
roronoa <2721423445@qq.com>
2018-02-26 09:14:58 +0800

    2018/02/25 9:14

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+class Index extends controller
+{
+    public function login()
+    {
+        return $this->fetch();
+    }
+
+    public function index()
+    {
+
+    }        
+
+    public function regist()
+    {
+        echo "后台的注册页面，应该用不着";
+    }
+}
+
---
+++ application/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+class Index extends Controller
+{
+    public function index()
+    {
+        echo "这里展示chinaren的登录页面";
+    }
+
+    public function regist()
+    {
+        echo "这里显示注册页面";
+    }
+}
